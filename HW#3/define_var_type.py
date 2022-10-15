52abb5891c95be77e4ee02808a2c5f294371bb34
Taras Tyzhuk <taras.tyzhuk@gmail.com>
2022-10-15 14:53:19 +0200

    vat type defined

---
+++ HW#3/define_var_type.py
@@ -0,0 +1,11 @@
+# 1. Define the id of next variables:
+int_a = 55 # integer
+str_b = 'cursor' # string
+set_c = {1, 2, 3} # set
+lst_d = [1, 2, 3] # list
+dict_e = {'a': 1, 'b': 2, 'c': 3} # dictionary
+print (id(int_a))
+print (id(str_b))
+print (id(set_c))
+print (id(lst_d))
+print (id(dict_e))
